4cb9a1936a22a5e5258b15e3c3e1bd6395f6f14c
whwlsgkr7 <whwlsgkr7@naver.com>
2024-03-21 22:25:40 +0900

    스프링부트 프로젝트 생성

---
+++ cleanplate/src/main/java/com/myproject/cleanplate/CleanplateApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.cleanplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanplateApplication.class, args);
+    }
+
+}
---
+++ cleanplate/src/test/java/com/myproject/cleanplate/CleanplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.cleanplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
