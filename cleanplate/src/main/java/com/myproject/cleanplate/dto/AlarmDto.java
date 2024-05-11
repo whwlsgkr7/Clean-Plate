a2867a7ffc4c4cb04e5e19e297c5346c2771152a
jinhack <whwlsgkr7@naver.com>
2024-05-12 01:57:54 +0900

    #20-알람 엔티티 수정
    알람 내용을 담을 content 필드 생성
    정적 팩토리 메서드를 만들고 객체의 동등성 관리와 해시 기반 컬렉션에서 올바르게 관리될 수 있게 equals and hashCode 작성

--- cleanplate/src/main/java/com/myproject/cleanplate/dto/AlarmDto.java
+++ cleanplate/src/main/java/com/myproject/cleanplate/dto/AlarmDto.java
@@ -2,27 +2,26 @@ package com.myproject.cleanplate.dto;
 
 import com.myproject.cleanplate.domain.Alarm;
 import com.myproject.cleanplate.domain.UserAccount;
-import com.myproject.cleanplate.domain.constant.AlarmArgs;
 import com.myproject.cleanplate.domain.constant.AlarmType;
 import lombok.Getter;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
-public record AlarmDto(Integer id,
+public record AlarmDto(Long id,
                        UserAccountDto userAccountDto,
                        AlarmType alarmType,
 //                       AlarmArgs alarmArgs,
-                       Timestamp createdAt,
-                       Timestamp modifiedAt,
-                       Timestamp removedAt
+                       LocalDateTime createdAt,
+                       LocalDateTime modifiedAt,
+                       LocalDateTime removedAt
                        ) {
 
     public static AlarmDto fromEntity(Alarm entity){
         return new AlarmDto(
                 entity.getId(),
-                UserAccountDto.from(entity.getUsername()),
+                UserAccountDto.from(entity.getUserAccount()),
                 entity.getAlarmType(),
-//                entity.getArgs(),
                 entity.getCreatedAt(),
                 entity.getModifiedAt(),
                 entity.getRemovedAt()
