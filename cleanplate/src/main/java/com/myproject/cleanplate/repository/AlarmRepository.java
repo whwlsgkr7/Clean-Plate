683d4f3e4b29442022af0571a41129ef681c5d17
jinhack <whwlsgkr7@naver.com>
2024-05-12 01:30:49 +0900

    #20-알람 리스트 조회 쿼리메서드 이름 변경
    Alarm 은 UserAccount 와 연관 관계가 매핑 되어있다.
    자동으로 UserAccount 의 pk값인 username 을 외래키로 관리하긴 하지만 AlarmRepository에서 연관 객체의 속성으로 Alarm 리스트를 조회하는 쿼리 메서드를 작성할 때는 연관 객체의 이름 + 연관 객체의 속성 형식으로 작성 해줘야 한다.

--- cleanplate/src/main/java/com/myproject/cleanplate/repository/AlarmRepository.java
+++ cleanplate/src/main/java/com/myproject/cleanplate/repository/AlarmRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Integer> {
-    List<Alarm> findAllByUsername(UserAccount username);
+    List<Alarm> findAllByUserAccountUsername(String username);
 
 }
